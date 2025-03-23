03c14bbfe73f83b96394130a1c0c4e76e9400935
cagatay246 <cagataykilgi7@gmail.com>
2025-03-23 12:53:16 +0300

    1

---
+++ CodeFirstRelation/Context/PatikaSecondDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirstRelation.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstRelation.Context
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        public PatikaSecondDbContext(DbContextOptions<PatikaSecondDbContext> options) : base(options)
+        {
+        }
+        //DbSetler
+        public DbSet<PostEntity> Posts => Set<PostEntity>();
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PostEntity>()
+                .HasOne(e => e.User) // Her postun bir kullanıcısı vardır.
+                .WithMany(e => e.Posts) // Her kullanıcının birden fazla postı olabilir.
+                .HasForeignKey(e => e.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
